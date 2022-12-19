c0f8291fe54454d7ab4066798a10b6d6bb7d3c68
Thijs Domburg <M.N.Domburg@student.tudelft.nl>
2022-12-19 11:51:09 +0100

    Added smooth path function from start to goal, improved visualisation

--- PDM_project/RRT/RRT.py
+++ PDM_project/RRT/RRT.py
@@ -2,7 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle, Circle
 from scipy import interpolate
-import time
 
 class RRT:
     #initializing self
@@ -11,11 +10,10 @@ class RRT:
         self.goal = goalpos
         self.mapdim = mapdim
         self.mapw, self.maph = self.mapdim
-        self.parent = []
         self.x = [startpos[0]]
         self.y = [startpos[1]]
         self.obsregion = []
-        self.edge = []
+        self.edge = [0]
         self.path = []
         self.goalfound = False
         self.max_iter = max_iter
@@ -42,7 +40,7 @@ class RRT:
         self.x.pop(n)
         self.y.pop(n)
 
-    def addedge(self, nparent, nchild):
+    def addedge(self, nparent):
         #print('Add edge between', nparent, ' and ', nchild)
         self.edge.append(nparent)
         
@@ -74,20 +72,17 @@ class RRT:
     def nodefree(self, n):
         #print('Check node ', n)
         if ((self.x[n], self.y[n]) in self.obsregion):
-
-            return False
-            
+            return False  
         else:
             return True  
 
     def edgefree(self, n1, n2):
         #print('Check edge between ', n1, "and", n2)
-        steps = np.linspace(0, 1, 30)
+        steps = np.linspace(0, 1, np.int64(self.distance(n1, n2)))
         for step in steps:
             x = np.int64(self.x[n1] * step + self.x[n2] * (1-step))
             y = np.int64(self.y[n1] * step + self.y[n2] * (1-step))
             if  (x, y) in self.obsregion:
-
                 return False
         return True
         
@@ -104,16 +99,36 @@ class RRT:
         n = len(self.x) - 1
         nnear = self.nearestnode(n)
         if self.edgefree(nnear, n) and self.nodefree(n) and (self.distance(n, nnear) <= self.d_search):
-            self.addedge(nnear, n)
+            self.addedge(nnear)
             if self.goaldistance(n) <= self.d_goal:
+                self.addnode(self.goal[0], self.goal[1])
+                self.addedge(n)
                 self.goalfound = True
         else: 
             
             self.removenode(n)
+
+    def reachcost(self, n):
+        nparent = self.edge[n]
+        cost = 0
+        while n != 0:
+            cost += self.distance(n, nparent)
+            n = nparent
+            nparent = self.edge[n]
+        return cost   
     
     def pathfound(self):
         if self.goalfound: 
+            self.path = []
+            ngoal = len(self.x)-1
+            self.path.append(ngoal)
+            nparent = self.edge[ngoal]
+            while nparent != 0:
+                self.path.append(nparent)
+                nparent = self.edge[nparent]
+            self.path.append(0)
             print("Goal found with", len(self.x), 'nodes after ', self.iters, " iterations.")
+            print("Cost to reach goal: ", self.reachcost(len(self.x)-1))
         return self.goalfound
 
     def iterations(self):
@@ -124,12 +139,34 @@ class RRT:
             print("No goal found after ", self.iters, " iterations :(")
             return False
     
+    def getpath(self):
+        path = []
+        
+        for i in self.path:
+            path.append((self.x[i], self.y[i]))
+        return path
+
+    def getsmoothpath(self):
+        self.smoothpath = []
+        if self.goalfound:
+            u = np.linspace(0, 1, 400)
+            x = []
+            y = []
+            coords = self.getpath()
+            for i in range(len(self.getpath())):
+                x.append(coords[i][0])
+                y.append(coords[i][1])
+            tck, _ = interpolate.splprep([x, y])
+            self.smoothpath = interpolate.splev(u, tck)
+        
+
+
     #Creating the map
     def makemap(self):    #Obstacles of form: [leftx, bottomy, Height, width]
         fig, ax = plt.subplots(1, 1)
 
         #Adding start and goal points to the map
-        ax.scatter(self.start[0], self.start[1])
+        ax.scatter(self.start[0], self.start[1], s = 100)
         ax.add_patch(plt.Circle(self.goal, self.d_goal, ec = '#FFA500', fill = False, lw = 5))
         #Adding obstacles to the map
         for i in range(len(self.obstacles)):
@@ -137,14 +174,27 @@ class RRT:
 
         #Adding nodes
         for i in range(1, len(self.x)):
-            ax.scatter(self.x[i], self.y[i], color = '#FF0000')
+            if i in self.path:
+                ax.scatter(self.x[i], self.y[i], color = '#FF0000')
+            else: 
+                ax.scatter(self.x[i], self.y[i], color = '#000000')
             #ax.text(self.x[i], self.y[i], i)
 
         #Adding edges
-        for i in range(len(self.edge)):
-            x_values = [self.x[i+1], self.x[self.edge[i]]]
-            y_values = [self.y[i+1], self.y[self.edge[i]]]
-            ax.plot(x_values, y_values, color = '000000')
+        for i in range(1, len(self.edge)):
+            x_values = [self.x[i], self.x[self.edge[i]]]
+            y_values = [self.y[i], self.y[self.edge[i]]]
+            if i in self.path:
+                ax.plot(x_values, y_values, color = '#FF0000')
+            else:
+                ax.plot(x_values, y_values, color = '#000000')
+
+        #Adding smooth path
+        self.getsmoothpath()
+        coords = self.smoothpath
+        if len(coords) != 0:
+            for i in range(len(coords[0])):
+                ax.scatter(coords[0][i], coords[1][i], color = '#00FF00')
 
         #Setting map size
         plt.xlim([0, self.mapw])
@@ -168,8 +218,6 @@ obstacles.append(np.array([130, 40, 20, 60], dtype = object))
 graph = RRT(start, goal, mapdim, dgoal, dsearch, obstacles, max_iter)
 graph.addobst()
 graph.makemap()
-iterations = 0
-max_iter = 20
 while not graph.pathfound() and graph.iterations():
     graph.expand()
 graph.makemap()
