021d7b620e583de0829b0709ab228fb2e49c3e69
Thijs Domburg <M.N.Domburg@student.tudelft.nl>
2022-12-22 21:09:30 +0100

    Added rewiring check function to improve optimality of path found, modified algorithm to let it search for better paths after finding the goal

--- PDM_project/RRT/RRT_star.py
+++ PDM_project/RRT/RRT_star.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle, Circle
 from scipy import interpolate
 import time
+from tqdm import tqdm
+
 
 class RRTstar:
     #initializing self
@@ -28,73 +30,50 @@ class RRTstar:
         self.iters= 0
         self.max_iter = max_iter
         #Initializing goalfound parameter
-        self.goalfound = False        
+        self.goalfound = False
+    
+    ####### Functions to create and remove edges and nodes #######
+
+    #Function to add a node to the list
+    def addnode(self, x, y):
+        self.x.append(x)
+        self.y.append(y)
+    
+    #Function to remove the nth node from the list
+    def removenode(self, n):
+        self.x.pop(n)
+        self.y.pop(n)
+
+    #Function to add an edge between the parent and the child node
+    def addedge(self, nparent):
+        self.parent.append(nparent)
+    
+    #Function to remove an edge
+    def removeedge(self, n):
+        self.parent.pop(n)
+    
+
+    ####### Functions to handle obstacles in the map #######
 
     #Function to check if a point is free of obstacles
     def isfree(self, x, y):
         free = True
         i = 0
         nobst = len(self.obstacles)
+        if (x == self.goal[0] and y == self.goal[1]) or (x == self.start[0] and y == self.start[1]):
+            free = False
         while free and i < nobst:
             if ((self.obstacles[i][0] - self.obsmargin) <= x <= (self.obstacles[i][2] + self.obsmargin)) and ((self.obstacles[i][1]- self.obsmargin) <= y <= (self.obstacles[i][3] + self.obsmargin)):
                 free = False
             i += 1
         return free
-        
-    #Function to add a node to the list
-    def addnode(self, x, y):
-        #print('Add node (', x, ", ", y, ")")
-        self.x.append(x)
-        self.y.append(y)
-    
-    #Function to remove the nth node from the list
-    def removenode(self, n):
-        #print('remove node ', n)
-        self.x.pop(n)
-        self.y.pop(n)
-
-    #Function to add an edge between the parent and the child node
-    def addedge(self, nparent):
-        #print("Added edge")
-        self.parent.append(nparent)
-    
-    #Function to remove an edge
-    def removeedge(self, n):
-        #print('remove edge ', n)
-        self.parent.pop(n)
-    
-    #Function to calculate the distance between nodes n1 and n2
-    def distance(self, n1, n2):
-        dx = (self.x[n1] - self.x[n2])
-        dy = (self.y[n1] - self.y[n2])
-        distance = np.sqrt(dx**2 + dy**2)
-        #print('Distance between ', n1, " and ", n2, ": ", distance)
-        return distance
-
-    #Function to calculate the distance to the endgoal
-    def goaldistance(self, n):
-        dx = (self.x[n] - self.goal[0])
-        dy = (self.y[n] - self.goal[1])
-        distance = np.sqrt(dx**2 + dy**2)
-        #print('Distance between ', n, " and the goal: ", distance)
-        return distance
-
-    #Function to calculate distances to each node
-    def distances(self, n):
-        distances = np.zeros(n)
-        for i in range(n):
-            distances[i] = self.distance(i, n)
-        #print("Node nearest to ", n, ": ", np.argmin(distances))
-        return distances
 
     #Function to check if the node is in the free space
     def nodefree(self, n):
-        #print('Check node ', n)
         return self.isfree(self.x[n], self.y[n])
 
     #Function to check if the edge is in the free space
     def edgefree(self, n1, n2):
-        #print('Check edge between ', n1, "and", n2)
         steps = np.linspace(0, 1, np.int64(self.distance(n1, n2)))
         for step in steps:
             x = np.int64(self.x[n1] * step + self.x[n2] * (1-step))
@@ -102,53 +81,29 @@ class RRTstar:
             if self.isfree(x, y) == False:
                 return False
         return True
-    
-    #Function to take a random sample in the map
-    def randomsample(self):
-        #print('Take random sample')
-        x = np.random.randint(0, self.mapw)
-        y = np.random.randint(0, self.maph)
-        return (x, y)
-    
-    #Function to expand the graph
-    def expand(self):
-        #print('Expand')
-        (x, y) = self.randomsample()
-        self.addnode(x, y)
-        n = len(self.x) - 1
-        distances = self.distances(n)
-        nnear = np.argmin(distances)
-        if self.edgefree(nnear, n) and (self.distance(n, nnear) <= self.d_search):
-            self.addedge(nnear)
-            cheapernode = self.cheapernodes(n)
-            if cheapernode != nnear:
-                #print("found cheaper node: ", cheapernode)
-                self.removeedge(n)
-                self.addedge(cheapernode)
-            if self.goaldistance(n) <= self.d_goal:
-                self.addnode(self.goal[0], self.goal[1])
-                self.addedge(n)
-                cheapernode = self.cheapernodes(n+1)
-                if cheapernode != n:
-                    self.removeedge(n+1)
-                    self.addedge(cheapernode)
-                self.goalfound = True
-        else: 
-            self.removenode(n)
-    
-    #Function to search for cheaper nodes to connect to
-    def cheapernodes(self, n):
-        cheapestparent = self.parent[n]
-        lowestcost = self.reachcost(n)
-        for i in range(n-1):
-            if self.distance(n, i) < self.d_cheaper:
-                if self.edgefree(n, i):
-                    cost = self.reachcost(i) + self.distance(n, i)
-                    if cost < lowestcost:
-                        cheapestparent = i
-                        lowestcost = cost
-        return cheapestparent
 
+    
+    ####### Functions to measure distances and costs ########
+
+    #Function to calculate the distance between nodes n1 and n2
+    def distance(self, n1, n2):
+        dx = (self.x[n1] - self.x[n2])
+        dy = (self.y[n1] - self.y[n2])
+        return np.sqrt(dx**2 + dy**2)
+
+    #Function to calculate distances to each node
+    def distances(self, n):
+        distances = np.zeros(n)
+        for i in range(n):
+            distances[i] = self.distance(i, n)
+        return distances
+
+    #Function to calculate the distance to the endgoal
+    def goaldistance(self, n):
+        dx = (self.x[n] - self.goal[0])
+        dy = (self.y[n] - self.goal[1])
+        return np.sqrt(dx**2 + dy**2)
+    
     #Function to calculate the cost to reach a node from the start
     def reachcost(self, n):
         nparent = self.parent[n]
@@ -158,56 +113,117 @@ class RRTstar:
             n = nparent
             nparent = self.parent[n]
         return cost   
-    
-    #Function to check if the goal is found, and to collect the nodes on the path
-    def pathfound(self):
-        if self.goalfound: 
-            self.path = []
-            ngoal = len(self.x)-1
-            self.path.append(ngoal)
-            nparent = self.parent[ngoal]
-            while nparent != 0:
-                self.path.append(nparent)
-                nparent = self.parent[nparent]
-            self.path.append(0)
-            print("Goal found with", len(self.x), 'nodes after ', self.iters, " iterations.")
-            print("Cost to reach goal: ", self.reachcost(len(self.x)-1))
-        return self.goalfound
 
+    #Function to take a random sample in the map
+    def randomsample(self):
+        x = np.random.randint(0, self.mapw)
+        y = np.random.randint(0, self.maph)
+        return (x, y)
+    
+
+    #######Functions to make the path optimal (RRT*)#######
+    
+    #Function to search for cheaper nodes to connect to
+    def cheapernodes(self, n, distances):
+        cheapestparent = self.parent[n]
+        lowestcost = self.reachcost(n)
+        for i in range(n-1):
+            if distances[i] < self.d_cheaper:
+                if self.edgefree(n, i):
+                    cost = self.reachcost(i) + self.distance(n, i)
+                    if cost < lowestcost:
+                        cheapestparent = i
+                        lowestcost = cost
+        return cheapestparent
+    
+    #Function to rewire nodes
+    def rewirecheck(self, n, distances):
+        for i in range(n-1):
+            if distances[i] < self.d_cheaper:
+                if self.edgefree(i, n):
+                    newcost = self.reachcost(n) + self.distance(n, i)
+                    if newcost < self.reachcost(i):
+                        self.parent[i] = n       
+
+
+    ####### Main functions used while iterating #######
+
+    #Function to expand the graph
+    def expand(self):
+        (x, y) = self.randomsample()
+        self.addnode(x, y)
+        n = len(self.x) - 1
+        distances = self.distances(n)
+        nnear = np.argmin(distances)
+        if self.edgefree(nnear, n) and (self.distance(n, nnear) <= self.d_search):
+            self.addedge(nnear)
+            cheapernode = self.cheapernodes(n, distances)
+            if cheapernode != nnear:
+                self.removeedge(n)
+                self.addedge(cheapernode)
+            self.rewirecheck(n, distances)
+
+            if self.goaldistance(n) <= self.d_goal:
+                if self.goalfound == False:
+                    print("Goal found after", self.iters, "iterations! Searching for better path...")
+                self.addnode(self.goal[0], self.goal[1])
+                self.addedge(n)
+                distances = self.distances(n+1)
+                cheapernode = self.cheapernodes(n+1, distances)
+                self.goalindex = n+1
+                if cheapernode != n:
+                    self.removeedge(n+1)
+                    self.addedge(cheapernode)
+                self.goalfound = True
+        else: 
+            self.removenode(n)
+             
     #Function to count the iterations
     def iterations(self):
         self.iters += 1
-        # if self.iters % 50 == 0:
-        #     print("iteration ", self.iters)
         if self.iters < self.max_iter:
             return True
         else: 
-            print("No goal found after ", self.iters, " iterations :(")
+            if self.goalfound:
+                print("Goal found with", len(self.x), 'nodes after ', self.iters, " iterations.")
+                print("Cost to reach goal: ", self.reachcost(len(self.x)-1))
+            else:
+                print("No goal found after ", self.iters, " iterations :( \nMade", len(self.x), "nodes.")
             return False
     
-    #Function to get the path to the goal
-    def getpath(self):
-        path = []
-        for i in self.path:
-            path.append((self.x[i], self.y[i]))
-        return path
+
+    ####### Functions that compute the final path from start to finish #######
+
+    #Function to collect the nodes and coordinates used for the path
+    def makepath(self):
+        if self.goalfound: 
+            self.path = []
+            self.pathy = [self.start[0]]
+            self.pathx = [self.start[1]]
+            self.path.insert(0, self.goalindex)
+            nparent = self.parent[self.goalindex]
+            while nparent != 0:
+                self.path.insert(1, nparent)
+                nparent = self.parent[nparent]
+            for i in range(1, len(self.path)):
+                if not ((self.x[self.path[i]] == self.x[self.path[i-1]]) and (self.y[self.path[i]] == self.y[self.path[i-1]])): 
+                    self.pathx.append(self.x[self.path[i]])
+                    self.pathy.append(self.y[self.path[i]])   
 
     #Function to get a smooth path to the goal
     def getsmoothpath(self):
         self.smoothpath = []
         if self.goalfound:
-            u = np.linspace(0, 1, 400)
-            x = []
-            y = []
-            coords = self.getpath()
-            for i in range(len(self.getpath())):
-                x.append(coords[i][0])
-                y.append(coords[i][1])
-            tck, _ = interpolate.splprep([x, y])
+            u = np.linspace(0, 1, 200)
+            tck, _ = interpolate.splprep([self.pathx, self.pathy])
             self.smoothpath = interpolate.splev(u, tck)
 
+
+    ####### Visualization #######
+
     #Function to visualize the map and graph
-    def makemap(self):   
+    def makemap(self, showpath = True, showrest = False, shownodes = False):   
+        self.makepath()
         fig, ax = plt.subplots(1, 1)
 
         #Adding start and goal points to the map
@@ -216,26 +232,29 @@ class RRTstar:
         #Adding obstacles to the map
         for i in range(len(self.obstacles)):
             ax.add_patch(Rectangle((self.obstacles[i][0], self.obstacles[i][1]), (self.obstacles[i][2] - self.obstacles[i][0]), (self.obstacles[i][3] - self.obstacles[i][1]), color = '#454545'))
-
+        
         # #Adding nodes to the map
-        # for i in range(1, len(self.x)):
-        #     if i in self.path:
-        #         ax.scatter(self.x[i], self.y[i], color = '#FF0000')
-        #     else: 
-        #         ax.scatter(self.x[i], self.y[i], color = '#000000')
+        if shownodes:
+            for i in range(1, len(self.x)):
+                if (i in self.path) and showpath:
+                    ax.scatter(self.x[i], self.y[i], color = '#FF0000')
+                elif showrest: 
+                    ax.scatter(self.x[i], self.y[i], color = '#000000')
 
         #Adding edges to the map
-        for i in range(1, len(self.parent)):
-            x_values = [self.x[i], self.x[self.parent[i]]]
-            y_values = [self.y[i], self.y[self.parent[i]]]
-            if i in self.path:
-                ax.plot(x_values, y_values, color = '#FF0000')
-            else:
-                ax.plot(x_values, y_values, color = '#000000')
+        if showrest or showpath:
+            for i in range(1, len(self.parent)):
+                x_values = [self.x[i], self.x[self.parent[i]]]
+                y_values = [self.y[i], self.y[self.parent[i]]]
+                if (i in self.path) and showpath:
+                    ax.plot(x_values, y_values, color = '#FF0000')
+                elif showrest:
+                    ax.plot(x_values, y_values, color = '#000000')
 
         #Adding smooth path to the map
         self.getsmoothpath()
         coords = self.smoothpath
+        # print("coords: ", coords)
         if len(coords) != 0:
             for i in range(len(coords[0])):
                 ax.scatter(coords[0][i], coords[1][i], color = '#00FF00')
@@ -243,57 +262,63 @@ class RRTstar:
         #Setting map size
         plt.xlim([0, self.mapw])
         plt.ylim([0, self.maph])
+    
 
         #Showing map
         plt.show()   
-    
+
+
 #Tests
-starttime = time.time()
 start = np.array([10, 10])
-goal = np.array([187, 15])
-mapdim = (200, 100) #145
+goal = np.array([187, 20])
+mapdim = (200, 145) #145
 dgoal = 10
 dsearch = 15
 dcheaper = 25
-max_iter = 10000
+max_iter = 20000
 obsmargin = 3
 obstacles = []
 
 ##Map 3
-# obstacles.append(np.array([25, 0, 30, 25], dtype = object))
-# obstacles.append(np.array([0, 55, 60, 60], dtype = object))
-# obstacles.append(np.array([55, 20, 60, 60], dtype = object))
-# obstacles.append(np.array([110, 25, 145, 30], dtype = object))
-# obstacles.append(np.array([85, 0, 90, 125], dtype = object))
-# obstacles.append(np.array([25, 85, 90, 90], dtype = object))
-# obstacles.append(np.array([25, 90, 30, 125], dtype = object))
-# obstacles.append(np.array([145, 55, 175, 60], dtype = object))
-# obstacles.append(np.array([55, 110, 60, 145], dtype = object))
-# obstacles.append(np.array([110, 25, 115, 145], dtype = object))
-# obstacles.append(np.array([145, 60, 150, 125], dtype = object))
-# obstacles.append(np.array([170, 0, 175, 55], dtype = object))
-# obstacles.append(np.array([150, 120, 180, 125], dtype = object))
-# obstacles.append(np.array([170, 95, 200, 100], dtype = object))
+obstacles.append(np.array([25, 0, 30, 25], dtype = object))
+obstacles.append(np.array([0, 55, 60, 60], dtype = object))
+obstacles.append(np.array([55, 20, 60, 60], dtype = object))
+obstacles.append(np.array([110, 25, 145, 30], dtype = object))
+obstacles.append(np.array([85, 0, 90, 125], dtype = object))
+obstacles.append(np.array([25, 85, 90, 90], dtype = object))
+obstacles.append(np.array([25, 90, 30, 125], dtype = object))
+obstacles.append(np.array([145, 55, 175, 60], dtype = object))
+obstacles.append(np.array([55, 110, 60, 145], dtype = object))
+obstacles.append(np.array([110, 25, 115, 145], dtype = object))
+obstacles.append(np.array([145, 60, 150, 125], dtype = object))
+obstacles.append(np.array([170, 0, 175, 55], dtype = object))
+obstacles.append(np.array([150, 120, 180, 125], dtype = object))
+obstacles.append(np.array([170, 95, 200, 100], dtype = object))
 
 ##MAP 2
-obstacles.append(np.array([30, 0, 35, 70], dtype = object))
-obstacles.append(np.array([30, 70, 110, 75], dtype = object))
-obstacles.append(np.array([105, 50, 110, 70], dtype = object))
-obstacles.append(np.array([135, 25, 140, 110], dtype = object))
-obstacles.append(np.array([75, 20, 175, 25], dtype = object))
-obstacles.append(np.array([75, 20, 80, 50], dtype = object))
-obstacles.append(np.array([160, 50, 200, 55], dtype = object))
-obstacles.append(np.array([160, 55, 165, 80], dtype = object))
+# obstacles.append(np.array([30, 0, 35, 70], dtype = object))
+# obstacles.append(np.array([30, 70, 110, 75], dtype = object))
+# obstacles.append(np.array([105, 50, 110, 70], dtype = object))
+# obstacles.append(np.array([135, 25, 140, 110], dtype = object))
+# obstacles.append(np.array([75, 20, 175, 25], dtype = object))
+# obstacles.append(np.array([75, 20, 80, 50], dtype = object))
+# obstacles.append(np.array([160, 50, 200, 55], dtype = object))
+# obstacles.append(np.array([160, 55, 165, 80], dtype = object))
 
 ##MAP 1
 # obstacles.append(np.array([40, 0, 50, 50], dtype = object))
-# obstacles.append(np.array([90, 50, 100, 100], dtype = object))
+# obstacles.append(np.array([110, 50, 120, 100], dtype = object))
 
 graph = RRTstar(start, goal, mapdim, dgoal, dsearch, dcheaper, obstacles, obsmargin, max_iter)
-#graph.addobst()
 graph.makemap()
-while not graph.pathfound() and graph.iterations():
+starttime = time.time()
+pbar = tqdm(total = max_iter)
+
+while graph.iterations():
     graph.expand()
+    pbar.update(1)
 endtime = time.time()
+
+pbar.close()
 print("Time elapsed: ", endtime - starttime)
-graph.makemap()
+graph.makemap(showpath = False, showrest = False, shownodes = False)
