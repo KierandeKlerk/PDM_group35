fdd61e5e218adc6c04d128a3a1bf6e7bbd2ea01f
Kieran de Klerk <k.j.deklerk-1@student.tudelft.nl>
2023-01-03 22:10:54 +0100

    Added __init__.py file in control subdirectory of quadrotor_project module, added a PID controller class file for the pointmass(based on the controller files of gym_pybullet_drones), added environment class PointMassAviary (based on the ones found in gym_pybullet_drones), updated pointmass.py to utilise the aforementioned controller and environment

--- PDM_project/pointmass.py
+++ PDM_project/pointmass.py
@@ -1,29 +1,86 @@
 import pybullet as p
 import time
 import pybullet_data
-import pkg_resources
 import numpy as np
+import pkg_resources
 
 from quadrotor_project.envs.PointMassAviary import PointMassAviary
-grav = 10
-physicsClient = p.connect(p.GUI)
-p.setAdditionalSearchPath(pybullet_data.getDataPath())
-p.setGravity(0,0,-grav)
-planeId = p.loadURDF("plane.urdf")
-obstacleId = p.loadURDF(pkg_resources.resource_filename('quadrotor_project', 'assets/drone_parcours.urdf'))
-startPos = [0,0,0.8]
-startOrientation = p.getQuaternionFromEuler([0,0,0])
-droneid = p.loadURDF(pkg_resources.resource_filename('quadrotor_project', 'assets/cf2p_pointmass.urdf'), startPos, startOrientation)
-m = 0.027
+from quadrotor_project.control.pointmassPID import pointmassPID
 
-for i in range (10000):
-    # p.applyExternalForce(droneid, -1, forceObj = [np.cos(3*i/700)/250, np.sin(3*i/700)/250, grav*m], posObj = [0,0,0], flags = p.LINK_FRAME)
-    p.applyExternalForce(droneid, -1, forceObj = [0, 0, grav*m], posObj = [0,0,0], flags = p.LINK_FRAME)
-    p.stepSimulation()
-    time.sleep(1./240.)
-cubePos, cubeOrn = p.getBasePositionAndOrientation(droneid)
-print(cubePos,cubeOrn)
-p.disconnect()
+from gym_pybullet_drones.utils.enums import DroneModel, Physics
+from gym_pybullet_drones.utils.utils import sync
 
-# env = PointMassAviary()
-# print(env.DRONE_MODEL.value)
+
+### Settings ###
+drone = DroneModel("cf2p")
+physics = Physics("dyn")
+gui = True
+aggregate = True
+obstacles = False
+simulation_freq_hz = 240
+control_freq_hz = 48
+duration_sec = 12
+output_folder = 'results'
+
+### Initial pose ###
+INIT_XYZ = np.array([0,0,0.8], dtype=float)
+INIT_RPY = np.array([0,0,0], dtype=float)
+AGGR_PHY_STEPS = int(simulation_freq_hz/control_freq_hz) if aggregate else 1
+
+### Target position calculation (to be replaced with data from RRT) ###
+R = 0.6
+
+TRACK_TIME = 3
+NUM_WAY_POINTS = TRACK_TIME*control_freq_hz
+print("Number of waypoints {}".format(NUM_WAY_POINTS))
+TARGET_POS = np.zeros((NUM_WAY_POINTS,3))
+for i in range(NUM_WAY_POINTS):
+    TARGET_POS[i, :] = [R*np.cos(i/NUM_WAY_POINTS*np.pi*2)-R, R*np.sin(i/NUM_WAY_POINTS*2*np.pi), 0.8]
+wp_counter = 0
+
+### Initiate environment ###
+env = PointMassAviary(drone_model=drone,
+                      initial_xyz=INIT_XYZ,
+                      initial_rpy=INIT_RPY,
+                      physics=physics,
+                      freq=simulation_freq_hz,
+                      aggregate_phy_steps=AGGR_PHY_STEPS,
+                      gui=gui,
+                      obstacles=obstacles,
+                      output_folder=output_folder)
+
+### Initiate PID controller ###
+ctrl = pointmassPID(drone_model=drone)
+
+### Run simulation ###
+CTRL_EVERY_N_STEPS = int(np.floor(env.SIM_FREQ/control_freq_hz))
+action = np.zeros(3)
+START = time.time()
+done = False
+
+for i in range(0, int(duration_sec*env.SIM_FREQ), AGGR_PHY_STEPS): 
+    ### Continue simulation if the goal hasn't been reached yet ###
+    
+    if not done:
+        
+        ### Step the simulation ###
+        state, done, _ = env.step(action)
+
+        ### Compute control at the desired  frequency
+        if i%CTRL_EVERY_N_STEPS == 0:
+
+            ### Compute control for the current waypoint ###
+            action = ctrl.computeControlFromState(control_timestep=CTRL_EVERY_N_STEPS*env.TIMESTEP,
+                                                  state=state,
+                                                  target_pos=TARGET_POS[wp_counter])
+
+            ### Select next waypoint ###
+            wp_counter = wp_counter + 1 if wp_counter < NUM_WAY_POINTS-1 else 0
+        ### Printout data ###
+        if i%env.SIM_FREQ == 0:
+            env.render()
+        
+        ### Sync the simulation ###
+        if gui:
+            sync(i, START, env.TIMESTEP)
+env.close()
