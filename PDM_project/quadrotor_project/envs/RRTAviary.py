f6b25736a95edace867199ff5c737120e543fa37
Kieran de Klerk <k.j.deklerk-1@student.tudelft.nl>
2023-01-12 15:51:39 +0100

    WIP started moving path generation into RRTAviary.py

--- PDM_project/quadrotor_project/envs/RRTAviary.py
+++ PDM_project/quadrotor_project/envs/RRTAviary.py
@@ -2,6 +2,11 @@ import os
 import numpy as np
 from datetime import datetime
 import pybullet as p
+import pybullet_data
+import pkg_resources
+from tqdm import tqdm
+import time
+
 
 from gym_pybullet_drones.envs.CtrlAviary import CtrlAviary
 from gym_pybullet_drones.utils.enums import DroneModel, Physics
@@ -13,16 +18,55 @@ class RRTAviary(CtrlAviary):
                  drone_model: DroneModel=DroneModel.CF2X,
                  num_drones: int=1,
                  neighbourhood_radius: float=np.inf,
-                 initial_xyzs=None,
-                 initial_rpys=None,
                  physics: Physics=Physics.PYB,
                  freq: int=240,
                  aggregate_phy_steps: int=1,
                  gui=False,
                  record=False,
-                 obstacles=False,
+                 obstacles = True,
+                 track = None,
+                 load_path = True,
+                 save_path = True,
                  user_debug_gui=True,
-                 output_folder='results'):
+                 output_folder='results',
+                 relative_path = 'PDM_group35/PDM_project/'):
+
+        self.TRACK = track
+        self.LOAD_PATH = load_path
+        self.RELATIVE_PATH = relative_path
+        self.SAVE_PATH = save_path
+
+        ### Setting up track variables ###
+        self.obstacle = pkg_resources.resource_filename('quadrotor_project', 'track{}.obj'.format(self.TRACK))
+        if self.TRACK == 0:
+            pybullet_data.getDataPath()
+            self.obstacle = "samurai.obj"
+            initial_xyzs = np.array([0,0,0.5], dtype = np.float64)
+            initial_rpys = np.array([0,0,0], dtype=np.float64)
+            
+        elif self.TRACK == 1:
+            initial_xyzs = np.array([0.5, 0.5, 0.8], dtype = np.float64)
+            initial_rpys = np.array([0,0,0], dtype=np.float64)
+            self.GOAL_XYZ = np.array([2, 5, 0.8], dtype = np.float64)
+            self.max_iter = 8000
+            self.track_time = 15
+        
+        elif self.TRACK == 2:
+            initial_xyzs = np.array([0.5, 0.5, 0.5], dtype = np.float64)
+            initial_rpys = np.array([0,0,0], dtype=np.float64)
+            self.GOAL_XYZ = np.array([6, 2, 2], dtype = np.float64)
+            self.max_iter = 30000
+            self.track_time = 18
+
+        elif self.TRACK == 3:
+            initial_xyzs = np.array([0.5, 0.5, 0.5], dtype = np.float64)
+            initial_rpys = np.array([0,0,0], dtype=np.float64)
+            self.GOAL_XYZ = np.array([4, 0.5, 0.5], dtype = np.float64)
+            self.max_iter = 130000
+            self.track_time = 50
+            
+
+                 
         
         super().__init__(drone_model=drone_model,
                          num_drones=num_drones,
@@ -37,6 +81,16 @@ class RRTAviary(CtrlAviary):
                          obstacles=obstacles,
                          user_debug_gui=user_debug_gui,
                          output_folder=output_folder)
+
+    ###############################################################################
+
+    def _addObstacles(self):
+        """Add obstacles to the environment.
+
+        These obstacles are loaded from standard URDF files included in Bullet.
+
+        """
+        self.OBSTACLEID = p.loadURDF(self.OBSTACLES[:-3]+"urdf")
 
     ###############################################################################
 
@@ -59,7 +113,7 @@ class RRTAviary(CtrlAviary):
                 self.IMG_PATH = os.path.join(self.OUTPUT_FOLDER, "recording_" + datetime.now().strftime("%d.%m.%Y_%H.%M.%S"), '')
                 os.makedirs(os.path.dirname(self.IMG_PATH), exist_ok=True)
         
-    ######################################################################################
+    ###############################################################################
 
     def _computeInfo(self):
         """Computes the current info dict(s).
